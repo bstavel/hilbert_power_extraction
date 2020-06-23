80c7949c6d4951dc1879c87e1d0d7ca78557959f
Brooke Staveland <bstavel@nx1.priv>
2020-06-23 09:06:52 -0700

    fix scp script, perm issues

--- scp_data.sh
+++ scp_data.sh
@@ -2,6 +2,6 @@
 
 ## scp data for subject ($0) to current location
 
-file_path="bstavel@nx1.neuro.berkeley.edu:/home/knight/deborahm/DataWorkspace/_projects/Dictator/Preprocessing/$0/Around_presentation/data_final_padding.mat"
+file_path="/home/knight/deborahm/DataWorkspace/_projects/Dictator/Preprocessing/$0/Around_presentation/data_final_padding.mat"
 
-scp $file_path .
+scp $file_path $1
