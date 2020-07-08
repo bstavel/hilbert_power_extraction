57cfe94acc7e1e6e6b9de1ab4bf4d941b6d11ff2
Brooke <bstavel@NEW-C02CV6C8ML85.local>
2020-07-08 09:19:12 -0700

    fix rsync for weird naming

--- tools/rsync_data.sh
+++ tools/rsync_data.sh
@@ -3,7 +3,7 @@
 ## scp data for subject ($1) to path ($2) ##
 
 # create file path, add presentation vs choice functionality next #
-file_path="bstavel@nx6.neuro.berkeley.edu:/home/knight/deborahm/DataWorkspace/_projects/Dictator/Preprocessing/$1/Around_presentation/part2_data_final_padding.mat"
+file_path="bstavel@nx6.neuro.berkeley.edu:/home/knight/deborahm/DataWorkspace/_projects/Dictator/Preprocessing/CPMC34/Around_presentation/part2_data_final_padding.mat"
 
 # if exists, scp #
 if test -f "$file_path"; then
