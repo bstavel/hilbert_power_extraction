813cfa2bf6e0b8f60a67957de27eca7147375612
Brooke Rhianne Staveland <bstavel@ln002.brc>
2020-07-14 17:21:47 -0700

    sbatch script

--- slurm/sbatch_hilbert_power_extraction.sh
+++ slurm/sbatch_hilbert_power_extraction.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=dhilbert_power_extraction_CP34
+#SBATCH --job-name=hilbert_power_extraction_IR19
 #SBATCH --account=fc_knightlab
 #SBATCH --partition=savio
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=00:60:00
+#SBATCH --time=02:00:00
 #SBATCH --mail-user=bstavel@berkeley.edu
 #SBATCH --mail-type=ALL
 #
--- tools/rsync_data.sh
+++ tools/rsync_data.sh
@@ -3,7 +3,7 @@
 ## scp data for subject ($1) to path ($2) ##
 
 # create file path, add presentation vs choice functionality next #
-file_path="bstavel@nx6.neuro.berkeley.edu:/home/knight/deborahm/DataWorkspace/_projects/Dictator/Preprocessing/CPMC34/Around_presentation/part2_data_final_padding.mat"
+file_path="bstavel@nx6.neuro.berkeley.edu:/home/knight/deborahm/DataWorkspace/_projects/Dictator/Preprocessing/${1}/Around_presentation/data_final_padding.mat"
 
 # do the rsync
 rsync -v --no-g $file_path "$2/${1}_data_final_padding.mat"
