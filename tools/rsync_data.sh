4cf6c837fa418224a89aa125099d56c91248dd22
Brooke <bstavel@NEW-C02CV6C8ML85.local>
2020-07-27 17:15:06 -0700

    are you sure you know bash

--- tools/rsync_data.sh
+++ tools/rsync_data.sh
@@ -3,12 +3,12 @@
 ## scp data for subject ($1) to path ($2), if ($3) is true, use choice ##
 
 
-if $3 == TRUE ; then
+if [$3 == TRUE ] ; then
   # create file path #
   file_path="bstavel@nx6.neuro.berkeley.edu:/home/knight/deborahm/DataWorkspace/_projects/Dictator/Preprocessing/${1}/Around_choice/data_final_choice_padding.mat"
   # do the rsync
   rsync -v --no-g $file_path "$2/${1}_data_final_choice_padding.mat"
-elif $3 == FALSE ; then
+elif [$3 == FALSE ] ; then
   # create file path #
   file_path="bstavel@nx6.neuro.berkeley.edu:/home/knight/deborahm/DataWorkspace/_projects/Dictator/Preprocessing/${1}/Around_presentation/data_final_padding.mat"
   # do the rsync
