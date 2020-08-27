010c632dd6609383627e593f86bc848926610016
Brooke Rhianne Staveland <bstavel@ln003.brc>
2020-08-26 19:14:57 -0700

    edits to job, change cluster on neuro cluster

--- slurm/sbatch_hilbert_power_extraction.sh
+++ slurm/sbatch_hilbert_power_extraction.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=hilbert_power_extraction_IR19
+#SBATCH --job-name=hilbert_power_extraction_choice
 #SBATCH --account=fc_knightlab
 #SBATCH --partition=savio
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=06:00:00
-#SBATCH --output=hilbert_job_%j.out
-#SBATCH --error=hilbert_job_%j.err
+#SBATCH --output=./slurm/hilbert_job_%j.out
+#SBATCH --error=./slurm/hilbert_job_%j.err
 #SBATCH --mail-user=bstavel@berkeley.edu
 #SBATCH --mail-type=ALL
 #
