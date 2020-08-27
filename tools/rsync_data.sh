777af9d069dbd4cc88de416745797d794ad961f3
Brooke <bstavel@NEW-C02CV6C8ML85.local>
2020-08-26 19:13:03 -0700

    new subs

--- tools/rsync_data.sh
+++ tools/rsync_data.sh
@@ -5,12 +5,12 @@
 
 if [ $3 == TRUE ] ; then
   # create file path #
-  file_path="bstavel@nx6.neuro.berkeley.edu:/home/knight/deborahm/DataWorkspace/_projects/Dictator/Preprocessing/${1}/Around_choice/data_final_choice_padding.mat"
+  file_path="bstavel@nx4.neuro.berkeley.edu:/home/knight/deborahm/DataWorkspace/_projects/Dictator/Preprocessing/${1}/Around_choice/data_final_choice_padding.mat"
   # do the rsync
   rsync -v --no-g $file_path "$2/${1}_data_final_choice_padding.mat"
 elif [ $3 == FALSE ] ; then
   # create file path #
-  file_path="bstavel@nx6.neuro.berkeley.edu:/home/knight/deborahm/DataWorkspace/_projects/Dictator/Preprocessing/${1}/Around_presentation/data_final_padding.mat"
+  file_path="bstavel@nx4.neuro.berkeley.edu:/home/knight/deborahm/DataWorkspace/_projects/Dictator/Preprocessing/${1}/Around_presentation/data_final_padding.mat"
   # do the rsync
   rsync -v --no-g $file_path "$2/${1}_data_final_padding.mat"
 fi
